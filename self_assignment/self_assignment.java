5ee14f1669893544063c4679cce2d2772a4b46fe
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-12-06 09:38:19 +0000

    自己代入(記述の省略)

--- self_assignment/self_assignment.java
+++ self_assignment/self_assignment.java
@@ -11,3 +11,26 @@ public class self_assignment {
 
   }
 }
+
+//記述の省略
+
+// class Main {
+//   public static void main(String[] args) {
+//     int number = 8;
+
+//     // 変数numberに7をかけて、変数numberを上書き
+//     // numberの記述を省略可（=の位置に気を付ける）
+//     number *= 7;
+
+//     // 変数numberを出力
+//     System.out.println(number);
+
+//     // 変数numberの値に1を足して、変数numberを上書き
+//     // 1の時だけは省略可
+//     number++;
+
+//     // 変数numberを出力
+//     System.out.println(number);
+
+//   }
+// }
