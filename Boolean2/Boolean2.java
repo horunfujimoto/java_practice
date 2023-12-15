bc8a8ee4610ccb76ab0ee031bbc191a821fd243f
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-12-15 06:45:28 +0000

    真偽値(計算)

---
+++ Boolean2/Boolean2.java
@@ -0,0 +1,10 @@
+public class Boolean2 {
+  public static void main(String[] args) {
+    // 「4 + 2」と「6」を比較し、falseとなるように
+    System.out.println(4 + 2 > 6);
+
+    // 「4 + 2」と「6」を比較し、trueとなるように
+    System.out.println(4 + 2 >= 6);
+
+  }
+}
