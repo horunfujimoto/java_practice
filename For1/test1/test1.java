301b11f20ee107f907929718a0f3b6da98922459
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-12-22 05:57:13 +0000

    for文

---
+++ For1/test1/test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+
+	public static void main(String[] args) {
+		// 1〜10の数字の偶数のみを小さい順に上から縦に並ぶように表示させる
+		// Iterate through numbers 1 to 10
+    for (int i = 2; i <= 10; i += 2) {
+        System.out.println(i);
+    }
+	}
+}
